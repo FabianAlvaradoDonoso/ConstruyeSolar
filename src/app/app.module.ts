6089dc3e650bd27b10fe3a6c8949a94de11b02f0
DiegoSepuSoto <diegosepu.soto@gmail.com>
2019-10-06 19:40:45 -0300

    Cambios en la comunicación con el servidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,43 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConexionService } from './conexion.service';
 import { ComponentsModule } from './components/components.module';
+import { ConexionService } from './conexion.service';
+
+import {
+  IMqttMessage,
+  MqttModule,
+  MqttService
+} from 'ngx-mqtt';
+
+export const MQTT_SERVICE_OPTIONS = {
+  connectOnCreate: true,
+  hostname: 'convive60.duckdns.org',
+  port: 80,
+  path: '/'
+};
+
+export function mqttServiceFactory() {
+  return new MqttService(MQTT_SERVICE_OPTIONS);
+};
+
 
 @NgModule({
-    declarations: [AppComponent],
-    entryComponents: [],
-    imports: [
-      BrowserModule,
-      IonicModule.forRoot(),
-      AppRoutingModule,
-      ComponentsModule
-    ],
-    providers: [
-        StatusBar,
-        SplashScreen,
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-        ConexionService
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ComponentsModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ConexionService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,101 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { ConexionService } from '../../conexion.service';
 import { ToastController } from '@ionic/angular';
 
+import { IMqttMessage, MqttModule, MqttService } from 'ngx-mqtt';
+import { Subscription } from 'rxjs';
+
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+    selector: 'app-inicio',
+    templateUrl: './inicio.page.html',
+    styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
 
-  constructor(private conexion: ConexionService, public toastController: ToastController) { }
+    constructor(public toastController: ToastController, private _mqttService: MqttService) { }
 
-  estado_websockets: string = "No es posible conectar";
-  informacion = {
-      humedad: ' - ',
-      temperatura: ' - ',
-      sensorMQ_9: ' - '
-  }
-  tempActual: string = "-Sin Datos-";
-  humedadActual: string = "-Sin Datos-";
-  alertasActual: string = "Sin alertas";
-  contadorAlertas: any = 0;
-  estado: any;
+    private subscription: Subscription;
+    private message: string;
+    informacion = {
+        humedad: ' - ',
+        temperatura: ' - ',
+        sensorMQ_9: ' - '
+    }
+    tempActual: string = "-Sin Datos-";
+    humedadActual: string = "-Sin Datos-";
+    alertasActual: string = "Sin alertas";
+    contadorAlertas: any = 0;
+    estado: any = "No es posible conectar";
 
-  async presentToastWithOptions(mensaje) {
-      const toast = await this.toastController.create({
-          header: mensaje,
-          // message: 'Click para Cerrar',
-          duration: 5000,
-          position: 'top',
-          buttons: [
-              {
-                  side: 'start',
-                  icon: 'warning',
-              },
-              {
-                  text: 'Cerrar',
-                  role: 'cancel',
-                  handler: () => {
-                      console.log('Cancel clicked');
-                  }
-              }
-          ],
-          color: 'light',
-      });
-      toast.present();
-  }
+    async presentToastWithOptions(mensaje) {
+        const toast = await this.toastController.create({
+            header: mensaje,
+            // message: 'Click para Cerrar',
+            duration: 5000,
+            position: 'top',
+            buttons: [
+                {
+                    side: 'start',
+                    icon: 'warning',
+                },
+                {
+                    text: 'Cerrar',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Cancel clicked');
+                    }
+                }
+            ],
+            color: 'light',
+        });
+        toast.present();
+    }
 
-  ngOnInit() {
-      this.estado = "";
-      this.conexion.conexionAlServidor().subscribe((mensaje: any) => {
-          if (mensaje.error_conexion) {
-              this.estado = mensaje.error_conexion;
-          }
+    ngOnInit() {
+        this.subscription = this._mqttService.observe('demostracion').subscribe((message: IMqttMessage) => {
+            this.message = JSON.parse(message.payload.toString());
+            this.estado = 'Leyendo información desde el servidor';
+            console.log(this.message);
+            // let arreglo = this.message.split("#");
+            // // this.informacion.contaminacion = arreglo[0];
+            // this.humedadActual = parseInt(arreglo[2]);
+            // this.tempActual = arreglo[1];
+            // // console.log(arreglo);
+        });
 
-          if (mensaje.desconexion) {
-              this.presentToastWithOptions(mensaje.desconexion);
-              console.log('Se ha desconectado el servidor');
-              this.estado_websockets = "Desconectado";
-          }
 
-          if (mensaje.conexion) {
-              this.estado = mensaje.conexion;
-              console.log(mensaje.conexion);
-              this.estado_websockets = "Conectado";
-              this.presentToastWithOptions(mensaje.conexion);
-              setInterval(() => {
-                  this.conexion.solicitar_data();
-              }, 2000)
-          }
-
-          if (mensaje.servidor)
-              console.log("Servidor: ", mensaje.servidor.mensaje);
-
-          if (mensaje.error_conexion) {
-              console.log(mensaje.error_conexion);
-              this.estado_websockets = "No es posible conectar";
-          }
-
-          if (mensaje.data_sensores) {
-              this.informacion.humedad = mensaje.data_sensores.data_sensores[0];
-              this.informacion.temperatura = mensaje.data_sensores.data_sensores[1];
-              this.informacion.sensorMQ_9 = mensaje.data_sensores.data_sensores[2];
-
-              console.log(mensaje.data_sensores.data_sensores);
-              this.tempActual = mensaje.data_sensores.data_sensores + "°C";
-              this.humedadActual = String(Number(mensaje.data_sensores.data_sensores) * 20 / 9.8).slice(0, 5) + "%";
-              if (Number(mensaje.data_sensores.data_sensores > 28)) {
-                  this.contadorAlertas += 1;
-              }
-              if (this.contadorAlertas > 0) {
-                  this.alertasActual = "Tienes alertas por revisar. Cantidad de alertas: " + String(this.contadorAlertas);
-              }
-          }
-
-      });
-
-  }
+    }
 
 }
